4f74601b179655af2df846b94b94b83810efb1bd
Simon Gysi <simon.gysi@gmail.com>
2021-03-01 22:12:04 +0100

    Make source code compatible with Ruby version 2.5
    The permitted_classes keyword argument was introduced to YAML.safe_load
    with Ruby version 2.6.

--- lib/natour/config.rb
+++ lib/natour/config.rb
@@ -5,14 +5,12 @@ module Natour
   class Config
     def self.load_file(filename, default: {}, dirs: [Dir.home, Dir.pwd])
       dirs.map do |dir|
-        begin
-          YAML.safe_load(
-            File.read(Pathname(dir).join(filename)),
-            permitted_classes: [Symbol]
-          )
-        rescue Errno::ENOENT
-          {}
-        end
+        YAML.safe_load(
+          File.read(Pathname(dir).join(filename)),
+          [Symbol]
+        )
+      rescue Errno::ENOENT
+        {}
       end.reduce(default, &:merge)
     end
   end
